ae6662947cb545481fb8c9e791483a96396ebf3b
Whit Nelson <whitnelson@gmail.com>
2025-05-01 15:33:19 -0400

    Adding group to user form

--- app/apps/users/forms.py
+++ app/apps/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import Group
 
 User = get_user_model()
 
@@ -15,18 +16,34 @@ class UserForm(forms.ModelForm):
         widget=forms.PasswordInput(),
         min_length=8,
         help_text="Password must be at least 8 characters long.",
+        required=False
     )
     confirm_password = forms.CharField(
-        widget=forms.PasswordInput(), label="Confirm Password"
+        widget=forms.PasswordInput(), 
+        label="Confirm Password",
+        required=False
+    )
+    group = forms.ModelChoiceField(
+        queryset=Group.objects.all(),
+        required=True,
+        label="User Type"
     )
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
+        fields = ["username", "email", "password", "group"]
         widgets = {
             "username": forms.TextInput(),
             "email": forms.EmailInput(),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and self.instance.pk:
+            # If editing an existing user, set the initial group
+            user_groups = self.instance.groups.all()
+            if user_groups.exists():
+                self.fields['group'].initial = user_groups.first()
 
     def clean(self):
         cleaned_data = super().clean()
@@ -40,7 +57,13 @@ class UserForm(forms.ModelForm):
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password"])
+        if self.cleaned_data.get("password"):
+            user.set_password(self.cleaned_data["password"])
+        
         if commit:
             user.save()
+            # Update user's group
+            user.groups.clear()
+            user.groups.add(self.cleaned_data["group"])
+            
         return user
