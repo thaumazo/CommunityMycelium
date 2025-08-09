5362853a82fc9a8787e6b75b74476522c2455a32
DanielLindenberger <dlindenb@mail.ubc.ca>
2025-08-09 11:56:21 -0700

    User social roles and maladaptives added, though some bugs. minor nav glitches fixed in base for highlighting active page

--- mycelium/apps/users/forms.py
+++ mycelium/apps/users/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from apps.communities.models import Community
 from apps.hats.models import Hat
+from apps.socialroles.models import Socialrole
+from apps.maladaptives.models import Maladaptive
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import Group
@@ -102,6 +104,20 @@ class UserForm(forms.ModelForm):
         label="Hats",
     )
 
+    user_socialroles = forms.ModelMultipleChoiceField(
+        queryset=Socialrole.objects.all(),
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "w-full"}),
+        required=False,
+        label="Social Roles",
+    )
+
+    user_maladaptives = forms.ModelMultipleChoiceField(
+        queryset=Maladaptive.objects.all(),
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "w-full"}),
+        required=False,
+        label="Maladaptive Schemas",
+    )
+
     linked_in = forms.URLField(
         required=False,
         widget=forms.TextInput(attrs={"class": "w-full"}),
@@ -132,6 +148,8 @@ class UserForm(forms.ModelForm):
             "invited_by",
             "user_communities",
             "user_hats",
+            "user_socialroles",
+            "user_maladaptives",
             "linked_in",
             # password fields are not model fields; they’re above
         ]
--- mycelium/apps/users/models.py
+++ mycelium/apps/users/models.py
@@ -20,6 +20,8 @@ class User(AbstractUser):
     )
 
     user_hats = models.ManyToManyField("hats.Hat", blank=True, related_name="users")
+    user_socials = models.ManyToManyField("socialroles.Socialrole", blank=True, related_name="users")
+    user_maladaptives = models.ManyToManyField("maladaptives.Maladaptive", blank=True, related_name="users")
 
     linked_in = models.URLField(blank=True, null=True)
 
