5ee85cf2aa7be9831cda99bbf70a4b1589987d77
DanielLindenberger <dlindenb@mail.ubc.ca>
2025-11-19 13:26:59 -0800

    Added donate button for Metachrysalis.org

---
+++ mycelium/config/context_processors.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+
+def site_settings(request):
+    """
+    Add site-wide settings to the template context.
+    """
+    return {
+        'URL_OVERRIDE': settings.URL_OVERRIDE,
+    }
